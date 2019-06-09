450247daa1d97717e7a48adc0a9e3ab6130505ba
itiievsk <itiyevskiy@gmail.com>
2019-06-09 21:09:03 +0300

    sign parser

--- computorV1.py
+++ computorV1.py
@@ -6,6 +6,13 @@ import re
 Token = collections.namedtuple("Token", ["type", "value"])
 
 
+def get_number(line: str):
+    if not line:
+        return 1
+    else:
+        return int(line)
+
+
 def tokenize(code):
     pow_0 = 0
     pow_1 = 0
@@ -15,28 +22,28 @@ def tokenize(code):
     token_specification = [
         (
             "EXPR",
-            r"(?P<sign>^|\-|\+|\=){1}((?P<number>\-?[0-9]+(?P<float>\.[0-9]+)?)?((?P<var>\*?x){1}((\^|\*\*)?(?P<power>[0-9]+)))?){1}",
+            r"(?P<sign>^|\-|\+|\=){1}((?P<number>\-?[0-9]+(?P<float>\.[0-9]+)?)?((?P<var>\*?x){1}((\^|\*\*)?(?P<power>[0-9]+)?))?){1}",
         ),
         ("MISMATCH", r"."),  # Any other character
     ]
     tok_regex = "|".join("(?P<%s>%s)" % pair for pair in token_specification)
     for mo in re.finditer(tok_regex, code):
-        print(mo.groups())
+        # print(mo.groups())
         kind = mo.lastgroup
         value = mo.group()
         if kind == "EXPR":
-            sign_multiplicator = (mo.group('sign') == '-') + right_side
             if mo.group('sign') == "=":
                 right_side = True
+
+            number = get_number(mo.group('number'))
+            sign_multiplicator = (mo.group('sign') == '-') + right_side
+
             if not mo.group('var') or (mo.group('power') and int(mo.group('power')) == 0):
-                pow_0 = pow_0 + (int(mo.group('number'))
-                                 * 1 ** sign_multiplicator)
+                pow_0 = pow_0 + (number * pow(-1, sign_multiplicator))
             elif mo.group('var') and (not mo.group('power') or int(mo.group('power')) == 1):
-                pow_1 = pow_1 + (int(mo.group('number'))
-                                 * 1 ** sign_multiplicator)
+                pow_1 = pow_1 + (number * pow(-1, sign_multiplicator))
             elif mo.group('var') and mo.group('power') and int(mo.group('power')) == 2:
-                pow_2 = pow_2 + (int(mo.group('number'))
-                                 * 1 ** sign_multiplicator)
+                pow_2 = pow_2 + (number * pow(-1, sign_multiplicator))
         elif kind == "MISMATCH":
             print(f"Unexpected value: {value!r}")
             return
